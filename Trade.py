7bb69da43bb36e559911cc38f0090e2c7b4706be
mertcantoglu <mertcantabakoglu@std.iyte.edu.tr>
2023-01-31 01:03:35 +0300

    buy  and emoji update

--- Trade.py
+++ Trade.py
@@ -3,6 +3,10 @@ class Trade:
         self.time = time
         self.type = _type
         self.value = value
+        self.numberOfEmoji = self.calcEmoji()
         
     def __str__(self) -> str:
-        return f"{self.time}\t{self.type}\t{self.value}"
+        return f"{self.time}\t{self.type}\t{self.value}\t{self.numberOfEmoji}"
+    
+    def calcEmoji(self):
+        return int(float(self.value.split(">")[1].split("$")[0].replace(",",""))/10)
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from logger import Logger
 from Trade import Trade
 from discord_webhook import DiscordWebhook, DiscordEmbed
 
-webhook = DiscordWebhook(url='https://discord.com/api/webhooks/1069398075749179464/qPNftdTIpsz2SnC2tHwo-njGbLQtir5jAn3UlqcAzptGcKWkFYtLQnX62_w84tO9GwCW')
+webhook = DiscordWebhook(url='https://discord.com/api/webhooks/1069711987354910831/tA_M5IwrWcw4DzrQrBK8TmbTRk77i1peKtu8j2XSh5JHGe-NEClC_XXXx12w-izX2nbi')
 DB = Database("O5O.db")
 logger = Logger()
 COLORS = {"buy" : "00c38c" ,"sell" : "f94d5c"}
@@ -29,27 +29,31 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), opti
 def send_message(obj: Trade):
     lines = obj.value.split("(")
     try:
-        embed = DiscordEmbed(title=f'$O5O {obj.type.upper()} @everyone', description=f'{lines[0]}\n{lines[1]}\n{obj.time}', color=COLORS[f"{obj.type}"])
+        embed = DiscordEmbed(title=f'$O5O {obj.type.upper()} @everyone', description=f'{obj.numberOfEmoji * "💰"}\n{lines[0]}\n{lines[1]}\n{obj.time}', color=COLORS[f"{obj.type}"])
         webhook.add_embed(embed)
         response = webhook.execute(remove_embeds=True)
     except:
-        pass
+        logger.printe("DiscordBotProblem" ,obj)
 
     
 
 while True:
-    driver.get(url)
-    time.sleep(2)
-    soup = BeautifulSoup(driver.page_source, 'lxml')
-    table = soup.find("div", { "class" : "ant-table-body" })
-    for row in table.findAll("tr")[::-1]:
-        cells = row.findAll("td")
-        transOBJ = Trade(cells[0].text,cells[1].text,cells[4].text.replace(")",""))
-        if DB.saveHash(transOBJ.value):
-            print(transOBJ)
-            send_message(transOBJ)
-            logger.printe("A new trade has been made, alert is being sent.")
-    time.sleep(60)  
+    try:
+        driver.get(url)
+        time.sleep(2)
+        soup = BeautifulSoup(driver.page_source, 'lxml')
+        table = soup.find("div", { "class" : "ant-table-body" })
+        for row in table.findAll("tr")[::-1]:
+            cells = row.findAll("td")
+            if cells[1].text == "buy":
+                transOBJ = Trade(cells[0].text,cells[1].text,cells[4].text.replace(")",""))
+                if DB.saveHash(transOBJ.value):
+                    print(transOBJ)
+                    send_message(transOBJ)
+                    logger.printe("A new trade has been made, alert is being sent.")
+        time.sleep(60) 
+    except:
+        logger.printe("Error something.")
           
             
         
